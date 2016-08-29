c89b2b05a5f73445bacb410f179dad0523cc625e
sedayoldas <sedaylds@windowslive.com>
2016-08-30 01:22:35 +0300

    Kronometre

---
+++ Kronometre/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Kronometre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblSaniye = new System.Windows.Forms.Label();
+            this.lblSalise = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblClear = new System.Windows.Forms.Label();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSaniye
+            // 
+            this.lblSaniye.AutoSize = true;
+            this.lblSaniye.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSaniye.Location = new System.Drawing.Point(54, 55);
+            this.lblSaniye.Name = "lblSaniye";
+            this.lblSaniye.Size = new System.Drawing.Size(32, 36);
+            this.lblSaniye.TabIndex = 0;
+            this.lblSaniye.Text = "0";
+            // 
+            // lblSalise
+            // 
+            this.lblSalise.AutoSize = true;
+            this.lblSalise.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSalise.Location = new System.Drawing.Point(135, 55);
+            this.lblSalise.Name = "lblSalise";
+            this.lblSalise.Size = new System.Drawing.Size(32, 36);
+            this.lblSalise.TabIndex = 1;
+            this.lblSalise.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(97, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 36);
+            this.label3.TabIndex = 2;
+            this.label3.Text = ":";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStop.Location = new System.Drawing.Point(72, 131);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 71);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "||";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStart.Location = new System.Drawing.Point(72, 131);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 71);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = ">";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblClear
+            // 
+            this.lblClear.AutoSize = true;
+            this.lblClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblClear.Location = new System.Drawing.Point(12, 313);
+            this.lblClear.Name = "lblClear";
+            this.lblClear.Size = new System.Drawing.Size(81, 25);
+            this.lblClear.TabIndex = 5;
+            this.lblClear.Text = "Temizle";
+            this.lblClear.Click += new System.EventHandler(this.lblClear_Click);
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStartStop.Location = new System.Drawing.Point(72, 220);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(75, 71);
+            this.btnStartStop.TabIndex = 6;
+            this.btnStartStop.Text = ">";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(220, 350);
+            this.Controls.Add(this.btnStartStop);
+            this.Controls.Add(this.lblClear);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblSalise);
+            this.Controls.Add(this.lblSaniye);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSaniye;
+        private System.Windows.Forms.Label lblSalise;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblClear;
+        private System.Windows.Forms.Button btnStartStop;
+    }
+}
+
---
+++ Kronometre/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kronometre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            salise++;
+            if (salise == 60)
+            {
+                saniye++;
+                salise = 0;
+            }
+            lblSaniye.Text = saniye.ToString();
+            lblSalise.Text = salise.ToString();
+            //sayılar Text özelliğine .ToStrint() dönüşümü yapılarak aktarılmalı.
+
+        }
+
+        int saniye = 0, salise = 0;//değişkenleri global olarak tanımlamak form içindeki tüm metotlarda kullanılabilmesini sağlar.
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();//timer1 artık interval çalışma aralığında tanımlanan sürede sürekli olarak çalıştırılacak.
+
+            //bir döngü başlatır.
+
+            btnStart.Visible = false;//görünürlüğünü kaldır
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();//timer ın yapmış olduğu işleri durdur.
+            btnStart.Visible = true;
+            //döngüyü break le
+        }
+
+        private void lblClear_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            saniye = 0; salise = 0;
+            btnStart.Visible = true;
+            lblSaniye.Text = saniye.ToString();
+            lblSalise.Text = salise.ToString();
+        }
+
+        int sayac = 0;
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            #region Sayısal Değer Kullanımı
+            //sayac++;
+            ////sayacın her tek değeri timer ı başlatır ve çift değeri durdurur.
+            //if (sayac % 2 == 1)
+            //{
+            //    timer1.Start();
+            //    btnStartStop.Text = "||";
+            //}
+            //else
+            //{
+            //    timer1.Stop();
+            //    btnStartStop.Text = ">";
+            //} 
+            #endregion
+            //......
+
+            #region Mantıksal Kullanımı
+            if (aktif == false)
+            {
+                timer1.Start();
+                aktif = true;
+                btnStartStop.Text = "||";
+            }
+            else
+            {
+                aktif = false;
+                timer1.Stop();
+                btnStartStop.Text = ">";
+            }
+            #endregion
+        }
+        bool aktif=false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
